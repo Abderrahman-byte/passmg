b4268830689dbc44fa5f96d28a6ba34a35896ffc
abderrahmane <el.abderrahman00@gmail.com>
2021-02-16 12:28:51 +0100

    Init database tables if it doesnt exists

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <sqlite3.h>
 #include "utils.h"
+#include "db.h"
 
 #define DB_PATH "./pass.db"
 
 int main() {
+	sqlite3 *db;
 	std::cout << "C++ " << __cplusplus << std::endl;
 
 	if(!file_exists(DB_PATH)) {
 		std::cout << "First Time" << std::endl;
+		init_db(DB_PATH, &db);
 	}
 
 	return 0;
