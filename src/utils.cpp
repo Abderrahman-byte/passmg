31e3a8f9be44a5ded675c1b6f546d0d2ccba4cd1
abderrahmane <el.abderrahman00@gmail.com>
2021-02-17 10:58:55 +0100

    Display authentication menu

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
 #include <string>
 #include <sys/stat.h>
 
 bool file_exists(const std::string& p) {
 	struct stat info;
 	return (stat(p.c_str(), &info) == 0) ;
+}
+
+void display_auth_menu() {
+	std::cout << "Authentication :" << std::endl;
+	std::cout << "[1] Login" << std::endl;
+	std::cout << "[2] Register" << std::endl;
+	std::cout << "[q] quit" << std::endl;
 }
--- src/utils.h
+++ src/utils.h
@@ -3,4 +3,5 @@
 #ifndef _PASS_UTILS_H_
 #define _PASS_UTILS_H_
 extern bool file_exists(const std::string&);
+extern void display_auth_menu();
 #endif
