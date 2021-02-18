6d3c75bf413fe77afc3a46ac94761f5c5924b4a4
abderrahmane <el.abderrahman00@gmail.com>
2021-02-18 10:49:46 +0100

    Pointer to string

--- src/utils.h
+++ src/utils.h
@@ -6,4 +6,5 @@ extern bool file_exists(const std::string&);
 extern void display_auth_menu();
 extern void disable_echo();
 extern void echo(bool);
+extern std::string convertToString(char *, int);
 #endif
