4d2d525da055d9e03d6188448ef532386961f846
abderrahmane <el.abderrahman00@gmail.com>
2021-02-18 10:45:22 +0100

    Move class to header file

--- src/models.h
+++ src/models.h
@@ -1,4 +1,32 @@
 #ifndef _MODELS_PASS_H_
 #define _MODELS_PASS_H_
-class User;
+class User {
+private :
+	std::string username;
+	std::string password;
+	std::string hashed_password;
+	bool anonymous;
+
+public :
+	User(std::string username, std::string password, std::string hashed_password) {
+		this->username = username;
+		this->password = password;
+		this->hashed_password;
+		this->anonymous = false;
+	}
+
+	User() {
+		this->anonymous = true;
+	}
+
+	User(User &u) {
+		this->username = u.username;
+		this->password = u.password;
+		this->anonymous = u.anonymous;
+	}
+
+	bool is_anonymous() {
+		return this->anonymous;
+	}
+};
 #endif
