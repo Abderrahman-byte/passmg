076eb5ef94bd4c4b179611f7752d23fba4219432
abderrahmane <el.abderrahman00@gmail.com>
2021-02-19 10:24:21 +0100

    Display user actions menu

--- src/utils.cpp
+++ src/utils.cpp
@@ -17,6 +17,22 @@ void display_auth_menu() {
 	std::cout << "[q] quit" << std::endl;
 }
 
+void display_actions_menu() {
+	// passwords CRUD operations
+	std::cout << "[1] List your passwords" << std::endl;
+	std::cout << "[2] Get a password" << std::endl;
+	std::cout << "[3] Generate new password" << std::endl;
+	std::cout << "[4] Delete password" << std::endl;
+	
+	// Account changes
+	std::cout << "[5] Change your username" << std::endl;
+	std::cout << "[6] Change master password" << std::endl;
+	std::cout << "[7] delete your account" << std::endl;
+
+	std::cout << "[8] logout" << std::endl;
+	std::cout << "[q] quit" << std::endl;
+}
+
 void echo(bool status=true) {
 	struct termios t;
 	tcgetattr(STDIN_FILENO, &t);
--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,7 @@
 #define _PASS_UTILS_H_
 extern bool file_exists(const std::string&);
 extern void display_auth_menu();
+extern void display_actions_menu();
 extern void disable_echo();
 extern void echo(bool);
 extern std::string convertToString(char *, int);
