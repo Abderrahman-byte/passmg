ac9ce5f3fd761ab01e37a8974d1c2b72deb62cda
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-01-28 12:49:08 +0100

    fix: move user_data to types file

---
+++ include/libpassmg/types.hpp
@@ -0,0 +1,20 @@
+#include <cstddef>
+#include <string>
+
+#ifndef _PASSMG_TYPES_
+#define _PASSMG_TYPES_
+
+struct password_t {
+        std::size_t id;
+        std::string title;
+        std::string content;
+};
+
+struct user_t {
+        std::size_t id;
+        std::string username;
+        std::string pw;
+        std::string hashed_pw;
+};
+
+#endif // !_PASSMG_TYPES_
